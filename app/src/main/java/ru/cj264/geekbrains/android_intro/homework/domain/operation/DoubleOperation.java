634665d086b9fdf8e319cad90810bf3ec995457a
mveynberg <mv@cj264.ru>
2021-03-16 15:28:58 +0500

    Calculate basic arithmetic operations

---
+++ app/src/main/java/ru/cj264/geekbrains/android_intro/homework/domain/operation/DoubleOperation.java
@@ -0,0 +1,19 @@
+package ru.cj264.geekbrains.android_intro.homework.domain.operation;
+
+public class DoubleOperation {
+    public static double add(double arg1, double arg2) {
+        return arg1 + arg2;
+    }
+
+    public static double sub(double arg1, double arg2) {
+        return arg1 - arg2;
+    }
+
+    public static double mul(double arg1, double arg2) {
+        return arg1 * arg2;
+    }
+
+    public static double div(double arg1, double arg2) {
+        return arg1 / arg2;
+    }
+}
--- app/src/main/java/ru/cj264/geekbrains/android_intro/homework/presenter/CalcPresenter.java
+++ app/src/main/java/ru/cj264/geekbrains/android_intro/homework/presenter/CalcPresenter.java
@@ -1,25 +1,35 @@
 package ru.cj264.geekbrains.android_intro.homework.presenter;
 
+import java.util.Locale;
+
 import ru.cj264.geekbrains.android_intro.homework.domain.Calc;
 import ru.cj264.geekbrains.android_intro.homework.domain.ICalc;
-import ru.cj264.geekbrains.android_intro.homework.ui.MainActivity;
+import ru.cj264.geekbrains.android_intro.homework.ui.ICalcView;
 
 public class CalcPresenter {
-    private final MainActivity mainActivity;
-    ICalc calc = new Calc();
+    private final ICalcView view;
+    private final ICalc calc = new Calc();
 
-    String expression = "";
+    private String expression = "";
 
-    public CalcPresenter(MainActivity mainActivity) {
-        this.mainActivity = mainActivity;
+    public CalcPresenter(ICalcView view) {
+        this.view = view;
+    }
+
+    public static String fmt(double d)
+    {
+        if(d == (long) d)
+            return String.format(Locale.US, "%d", (long) d);
+        else
+            return String.format(Locale.US, "%s", d);
     }
 
     public void buttonPressed(String symbol) {
         if (symbol.equals("=")) {
+            expression = fmt(calc.evaluateExpression(expression));
         } else {
             expression += symbol;
-
-            mainActivity.displayResult(expression);
         }
+        view.displayResult(expression);
     }
 }
